018c0d0a7ba5827d091e5dc2ed6f88a7316beaf1
lemanshamilova <lemansamilova2003@gmail.com>
2025-01-28 02:17:27 +0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { ToastContainer, toast } from "react-toastify";
+
+function App() {
+  return (
+    <>
+      <ToastContainer />
+      <Header />
+      <main className="min-h-[calc(100vh-120px)]">
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/common/index.js
@@ -0,0 +1,11 @@
+const backendDomain = "http://localhost:8080";
+
+const SummaryApi = {
+  singUP: {
+    url: `${backendDomain}/api/signup`,
+    method: "post",
+  },
+};
+
+
+export default SummaryApi;
